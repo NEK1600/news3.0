09abb2e25a5cf2994ddd077c274fc7edc12d14d2
NEK1600 <nikitajw0000@gmail.com>
2022-11-26 12:56:11 +1000

    доработки

--- lib/ui/home_news.dart
+++ lib/ui/home_news.dart
@@ -3,32 +3,26 @@ import 'package:news3_0/data/model/news_model.dart';
 import 'package:news3_0/data/remote/api_client.dart';
 import 'package:provider/provider.dart';
 
-class HomeNewsModel extends ChangeNotifier{
-  int indexL=1;
+class HomeNewsModel extends ChangeNotifier {
+  int indexL = 1;
   late Future<NewsModel> newsModelList;
 
   int get getIndex => indexL;
   Future<NewsModel> get getNewsList => newsModelList;
 
-  void funIn(int index){
+  void funIn(int index) {
     indexL = index;
     print('Индекс ${getIndex}');
-    notifyListeners();
-  }
-  void funList(Future<NewsModel> newsModel){
-    newsModelList = newsModel;
     notifyListeners();
   }
 }
 
 class HomeNewsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    var newsRepositoryProvider = Provider.of<ApiClient>(context,listen: false);
-    var homeProv = Provider.of<HomeNewsModel>(context,listen: false);
+    var newsRepositoryProvider = Provider.of<ApiClient>(context, listen: false);
+    var homeProv = Provider.of<HomeNewsModel>(context, listen: false);
     homeProv.newsModelList = newsRepositoryProvider.searchNews('');
-
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: PreferredSize(
@@ -36,49 +30,11 @@ class HomeNewsScreen extends StatelessWidget {
           child: AppBar(),
         ),
         body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Container(
-                    color: Color(0xFF393939),
-                    child: ExpansionTile(
-                        backgroundColor: Color(0xFF393939),
-                        title: const Center(child: Text("Фильтр слово", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),)),
-                        children: <Widget>[
-                          TextFormFieldWidget(),
-                        ])),
-                Expanded(
-                  flex: 10,
-                  child: Consumer<ApiClient>(
-                    builder: (context, model, child) {
-                      return FutureBuilder(
-                        future: homeProv.newsModelList,
-                        builder:(context, snapshot){
-                          if (snapshot.hasData) {
-                            return ListView.builder(
-                              itemCount: model.newsmModel!.articles!.length,
-                              itemBuilder: (context, index) => GestureDetector(
-                                child: CardNew(title: model.newsmModel!.articles[index]!.title,),
-                                onTap: () {
-                                  homeProv.funIn(index);
-                                  Navigator.pushNamed(context, '/one/description',
-                                    arguments: {
-                                    "index" : homeProv.getIndex,
-                                    "newsList": homeProv.getNewsList});
-                                },
-                              ),
-                            );
-                          } else if (snapshot.hasError) {
-                            return Text('${snapshot.error}');
-                          }
-                          return const CircularProgressIndicator();
-                        },
-                      );
-
-                    },
-                  )
-                ),
-              ])
-        );
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextFormFieldWidget(),
+              ListBuilderWidget(),
+            ]));
   }
 }
 
@@ -86,28 +42,78 @@ class TextFormFieldWidget extends StatelessWidget {
   final _wordException = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    var homeProv = Provider.of<HomeNewsModel>(context,listen: false);
-    var newsRepositoryProvider = Provider.of<ApiClient>(context,listen: false);
-    return TextFormField(
-      controller: _wordException,
-      decoration: const InputDecoration(
-          hintStyle: TextStyle(
-              fontSize: 17,
-              color: Color(0xFF7D7D7D)),
-          filled: true,
-          fillColor: Color(0xFF202020),
-          hintText: 'ключевое слово'
-      ),
-      onFieldSubmitted: (price) {
-        homeProv.newsModelList = newsRepositoryProvider.searchNews(price);
-      },
-    );
+    var homeProv = Provider.of<HomeNewsModel>(context, listen: false);
+    var newsRepositoryProvider = Provider.of<ApiClient>(context, listen: false);
+    return Container(
+        color: Color(0xFF393939),
+        child: ExpansionTile(
+            backgroundColor: Color(0xFF393939),
+            title: const Center(
+                child: Text(
+              "Фильтр слово",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )),
+            children: <Widget>[
+              TextFormField(
+                controller: _wordException,
+                decoration: const InputDecoration(
+                    hintStyle:
+                        TextStyle(fontSize: 17, color: Color(0xFF7D7D7D)),
+                    filled: true,
+                    fillColor: Color(0xFF202020),
+                    hintText: 'ключевое слово'),
+                onFieldSubmitted: (price) {
+                  homeProv.newsModelList = newsRepositoryProvider.searchNews(price);
+                },
+              )
+            ]));
   }
 }
 
-class CardNew extends StatelessWidget {
+class ListBuilderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext contextM) {
+    var homeProv = Provider.of<HomeNewsModel>(contextM, listen: false);
+    return Expanded(
+        flex: 10,
+        child: Consumer<ApiClient>(
+          builder: (context, model, child) {
+            return FutureBuilder(
+              future: homeProv.newsModelList,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: model.newsmModel!.articles!.length,
+                    itemBuilder: (context, index) => GestureDetector(
+                      child: CardNewWidget(
+                        title: model.newsmModel!.articles[index]!.title,
+                      ),
+                      onTap: () {
+                        homeProv.funIn(index);
+                        Navigator.pushNamed(context, '/one/description',
+                            arguments: {
+                              "index": homeProv.getIndex,
+                              "newsList": homeProv.getNewsList});
+                      },
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            );
+          },
+        ));
+  }
+}
+
+class CardNewWidget extends StatelessWidget {
   final String title;
-  CardNew({required this.title,});
+  CardNewWidget({required this.title,});
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -119,10 +125,8 @@ class CardNew extends StatelessWidget {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25), maxLines: 3),
+                Text(title, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25), maxLines: 3),
               ])),
     );
   }
 }
-
-
